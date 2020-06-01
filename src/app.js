deef7c228e9b071bc4692b0ec094e22effef873d
reddevilcero <reddevil_cero@hotmail.com>
2020-06-01 09:53:50 +0100

    react app basic structure

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-alert('connected')
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+  return (
+    <div className="main-container">
+      <h1>Image gallery</h1>
+      <div className="infinite-bar"></div>
+      <div className="sign">
+        <p>Create by: Pedro David Garcia Lopez 02/06/2020</p>
+      </div>
+    </div>
+  );
+};
+
+render(<App/>, document.getElementById('root'))
