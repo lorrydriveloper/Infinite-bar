6d6b8ce51ab700f8de790bb6ea5c58678a3089c4
reddevilcero <reddevil_cero@hotmail.com>
2020-06-01 19:13:03 +0100

    version v2 with infinite loop on images

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
 import InfiniteBar from "./components/InfiniteBar";
+import InfiniteBarV2 from "./components/infiniteBarV2";
 
 const App = () => {
   return (
     <div className="main-container">
       <h1>Image gallery</h1>
       <div className="infinite-bar">
-        <InfiniteBar/>
+        <InfiniteBar />
+      </div>
+      <div className="infinite-bar">
+        <InfiniteBarV2 />
       </div>
       <div className="sign">
         <p>Create by: Pedro David Garcia Lopez 02/06/2020</p>
---
+++ src/components/infiniteBarV2.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+
+function tranformCSS(){
+  let left = 0;
+  let initialLeft = 0;
+  let i = 0;
+  let translateX = 3750;
+
+  const moveleft = setInterval(() => {
+    let images = document.querySelectorAll(".logo-container");
+    let track = document.querySelector(".trackv2");
+    // it gives dynamically absolute positioning images a left attribute
+    images.forEach((node, i) => {
+      node.style.left = initialLeft - left + "px";
+      initialLeft += 250;
+      if (i === 14) {
+        initialLeft = 0;
+      }
+    });
+
+    if (left % 250 === 1 && track.children[i]) {
+      track.children[i].style.transform = `translateX(${translateX}px)`;
+      i++;
+      if (i === 15) {
+        i = 0;
+        translateX += 3750;
+      }
+    }
+
+    left += 1;
+  }, 20);
+
+  return () => clearInterval(moveleft);
+}
+
+const InfiniteBarV2 = () => {
+  let [logos, setLogos] = useState([]);
+  fetch(
+    "https://w6jnvo997d.execute-api.eu-west-2.amazonaws.com/test/technical-test"
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      logos = json.body.urls;
+      setLogos(json.body.urls);
+    });
+
+  useEffect(tranformCSS, []);
+
+  return (
+    <div className="track trackv2">
+      {logos.map((url, index) => {
+        return (
+          <div key={index} className="logo-container logo-continerV2">
+            <img src={url} alt="logo" />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default InfiniteBarV2;
