912391bea2a9a88c9be746e4b9b4d22ef520126e
reddevilcero <reddevil_cero@hotmail.com>
2020-06-04 18:56:34 +0100

    refactor of InfiniteBarV2.

--- src/components/infiniteBarV2.js
+++ src/components/infiniteBarV2.js
@@ -1,37 +1,5 @@
 import React, { useState, useEffect } from "react";
-
-function tranformCSS() {
-  let left = 0;
-  let initialLeft = 0;
-  let i = 0;
-  let translateX = images.length * 250;
-
-  const moveleft = setInterval(() => {
-    let images = document.querySelectorAll(".logo-container");
-    let track = document.querySelector(".trackv2");
-    // it gives dynamically absolute positioning images a left attribute
-    images.forEach((node, i) => {
-      node.style.left = initialLeft - left + "px";
-      initialLeft += 250;
-      if (i === images.length - 1) {
-        initialLeft = 0;
-      }
-    });
-
-    if (left % 250 === 1 && track.children[i]) {
-      track.children[i].style.transform = `translateX(${translateX}px)`;
-      i++;
-      // end of images and new loop start.
-      if (i === images.length) {
-        i = 0;
-        translateX += images.length * 250;
-      }
-    }
-    left += 1;
-  }, 20);
-
-  return () => clearInterval(moveleft);
-}
+import { createPortal } from "react-dom";
 
 const InfiniteBarV2 = () => {
   let [logos, setLogos] = useState([]);
@@ -43,7 +11,46 @@ const InfiniteBarV2 = () => {
       setLogos(json.body.urls);
     });
 
-  useEffect(tranformCSS, []);
+  useEffect(() => {
+    let left = 0;
+    let initialLeft = 0;
+    let i = 0;
+    let translateX = 0;
+
+    const moveleft = setInterval(() => {
+      let images = document.querySelectorAll(".logo-container");
+      let track = document.querySelector(".trackv2");
+      // it gives dynamically absolute positioning images a left attribute
+      images.forEach((node, i) => {
+        node.style.left = initialLeft - left + "px";
+        initialLeft += node.getBoundingClientRect().width;
+        if (i === images.length - 1) {
+          initialLeft = 0;
+        }
+      });
+
+      if (track.children[i]) {
+        let element = track.children[i].getBoundingClientRect();
+        if (element.x + element.width < 0) {
+          const totalWidth = Array.from(images).reduce((sum, image) => {
+            return image.getBoundingClientRect().width + sum;
+          }, 0);
+          translateX = translateX >= totalWidth ? translateX : totalWidth;
+          track.children[i].style.transform = `translateX(${translateX}px)`;
+          i++;
+          // end of images and new loop start.
+          if (i === images.length) {
+            i = 0;
+            translateX += totalWidth;
+          }
+        }
+      }
+
+      left += 1;
+    }, 5);
+
+    return () => clearInterval(moveleft);
+  }, []);
 
   return (
     <div className="track trackv2">
